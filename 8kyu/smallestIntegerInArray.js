2120152f32929a5224ccf106edfe2e47df4c9a66
Daniel Ezekiel <dbezekiel77@gmail.com>
2022-05-02 11:30:31 +0100

    refactored the code for solving the smallest integer in array challenge

--- 8kyu/smallestIntegerInArray.js
+++ 8kyu/smallestIntegerInArray.js
@@ -5,28 +5,27 @@
 // Given [34, 15, 88, 2] your solution will return 2
 // Given [34, -345, -1, 100] your solution will return -345
 
+// class SmallestIntegerFinder {
+//     findSmallestInt(args) {
+//       let smallest = args[0];
+//       for (let i = 0; i < args.length; i++) {
+//           if(smallest > args[i]) {
+//               smallest = args[i];
+//             } else {
+//                 smallest = smallest;
+//             }
+//         }
+//       return smallest
+//     }
+// }
+  
+// The more effcient method involves calling the sort method on the array and after sorting the array, the first element which is the lowest element is then returned
 class SmallestIntegerFinder {
     findSmallestInt(args) {
-      let smallest = args[0];
-      for (let i = 0; i < args.length; i++) {
-          if(smallest > args[i]) {
-              smallest = args[i];
-            } else {
-                smallest = smallest;
-            }
-        }
-      return smallest
+  //the array is first sorted from the smallest integer to the highest;
+      args.sort( (a, z) => a - z );
+      
+  //the sorted value is then stored inside the array called args, the smallest value can then be retrieved from the zero-th index of the array
+      return args[0];
     }
 }
-  
-// function findSmallestInt(args) {
-//   let smallest = args[0];
-//   for (let i = 0; i < args.length; i++) {
-//    if(smallest > args[i]) {
-//      smallest = args[i];
-//    } else {
-//      return
-//    }
-//   }
-//   return smallest
-// }
