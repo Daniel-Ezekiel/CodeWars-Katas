844751c1cd04b26db0fa39e0df3c9464ee1a26a1
Daniel Ezekiel <dbezekiel77@gmail.com>
2022-05-03 12:07:00 +0100

    pushed newly completed challenge

---
+++ 8kyu/makeUpperCase.js
@@ -0,0 +1,17 @@
+/**************
+  PREP
+  Challenge: To convert each character within a string into uppercase
+  
+  P -  string
+  R - return, not console.log(), the string in uppercase
+  E - str = hello, return HELLO,
+      str = danno, return DANNO,
+      str = fAssH, return FASSH.
+  
+  
+  P - the .toUpperCas() method is the best method to call on the string to solve this problem
+**************/
+
+function makeUpperCase(str) {
+  return str.toUpperCase();
+}
