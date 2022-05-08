04179a68b8c5feea3a4a0d3e321f375c97c8ce7a
Daniel Ezekiel <dbezekiel77@gmail.com>
2022-05-08 15:41:12 +0100

    completed sentenceSmash challenge

---
+++ 8kyu/sentenceSmash.js
@@ -0,0 +1,9 @@
+// Challenge: To join all the strings in an array into a sentence(Sentence Smash)
+// This will be acheived using the join method with the argument of a space within the strings like so ' ';
+
+function smash (words) {
+    return words.join(' ')
+};
+ 
+// Alternative: using a function expression and an arrow function
+// const smash = words => words.join(' ');
