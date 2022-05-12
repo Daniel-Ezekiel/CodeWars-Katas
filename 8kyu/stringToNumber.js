f52c4a85fe22396a51eba21fa3bccd6986edac75
Daniel Ezekiel <dbezekiel77@gmail.com>
2022-05-12 23:53:34 +0100

    pushed newly committed stringToNumber challenge

---
+++ 8kyu/stringToNumber.js
@@ -0,0 +1,9 @@
+// Challenge: to return a number string as a number
+// This can be done by using the Number method and passing the string as the parameter
+
+var stringToNumber = function(str){
+    return Number(str);
+}
+  
+// Alternative using a function expression and an arrow function
+// const stringToNumber = str => Number(str)
