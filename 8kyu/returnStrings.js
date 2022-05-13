be45952482b9b54238a6e2fafa8a9354dc5037f3
Daniel Ezekiel <dbezekiel77@gmail.com>
2022-05-13 02:21:42 +0100

    pushed the returnStrings challenge

---
+++ 8kyu/returnStrings.js
@@ -0,0 +1,8 @@
+// Challenge: To write a function that returns a given string after inserting the parameter of a name
+
+function greet(name){
+    return `Hello, ${name} how are you doing today?`
+}
+
+// Alternative: using a function expression and an arrow funtion
+// const greet = name => `Hello, ${name} how are you doing today?`
