1eb1a60b9544a65b7182688c52edd63014221ae2
Daniel Ezekiel <dbezekiel77@gmail.com>
2022-05-24 23:41:10 +0100

    successfully coded on pressure

---
+++ 8kyu/youCantCodeUnderPressure.js
@@ -0,0 +1,22 @@
+/*************
+  PREP
+  Challenge: To double an integer and return the result
+  
+  P - strictly an integer
+  R - return, not console.log(), the double
+  E - if integer is 4, returbn 8;
+         integer is 50, return 100;
+         integer is 1250, return 2500.
+  
+  P - could store the value of the integer double in a variable and then return the result, or return the result of the double straight up 
+*************/
+
+function doubleInteger(i) {
+    //   const integerDouble = i*2
+    //   return integerDouble;
+    
+    return i*2
+}
+
+// Good thing my code was on top of pressure, I don't think I would have completed it under pressure, PHEWWWWW!!
+// PRESSUUUREEEEE!!!!!!!!!!!!
