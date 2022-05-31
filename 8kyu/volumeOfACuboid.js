a7a795d994d0cef86fdaa9cfe1ab115a6a5b67dd
Daniel Ezekiel <dbezekiel77@gmail.com>
2022-05-31 20:02:13 +0100

    volume of a cuboid chalenge

---
+++ 8kyu/volumeOfACuboid.js
@@ -0,0 +1,17 @@
+/*******************
+  PREP
+  Challenge: To return the volume of a cuboid;
+  P - three numbers as params;
+  R - return, not console.log(), the volume
+  E - (1,2,2), 4;
+      (6.3,2,5), 63;
+  P - multiply the three param values;
+      returns the product as the result;
+*******************/
+
+class Kata {
+    static getVolumeOfCuboid(length, width, height) {
+      // your code here
+      return length * width * height;  
+    }
+}
