2d1c12b7790e6e428c5b8a94f4d060ac80ecd6a4
Daniel Ezekiel <dbezekiel77@gmail.com>
2022-06-03 22:42:28 +0100

    completed another challenge

---
+++ 8kyu/function3MultiplyingTwoNumbers.js
@@ -0,0 +1,16 @@
+/*******************
+  PREP
+  Challenge: To multiply two numbers;
+  P - two numbers;
+  R - return, not console.log(), a product
+  E - (2, 3), 6;
+      (2, 8), 16;
+      (8, 3), 24;
+  P - create a function,
+      takes two params,
+      within the code block, multiply param1 with param2,
+      return the product;
+*******************/
+
+// Write here your multiply-function
+const multiply = (n1, n2) => n1*n2;
