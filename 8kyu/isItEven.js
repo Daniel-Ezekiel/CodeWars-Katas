26a066647a57864070f4ca14758288590ff52690
Daniel Ezekiel <dbezekiel77@gmail.com>
2022-06-17 23:18:24 +0100

    new challenge completed

---
+++ 8kyu/isItEven.js
@@ -0,0 +1,17 @@
+/********************
+  Challenge: To check if a given number is even or not
+  
+   P - one number;
+   R - return, true or false if even or not;
+   E - (0), true;
+       (-4), true;
+       (1), false;
+       (0.5), false;
+       
+   P - use the modulus operator to confirm if the number is even or not and return true or false;
+********************/
+
+function testEven(n) {
+    //Your awesome code here!
+  return !(n%2);
+}
