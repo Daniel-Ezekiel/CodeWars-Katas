b6f44e66f01f74697ccd2719ea8bcc549a8cc3d1
Daniel Ezekiel <dbezekiel77@gmail.com>
2022-06-18 21:52:08 +0100

    new challenge

---
+++ 8kyu/grasshopperDebugSayHello.js
@@ -0,0 +1,14 @@
+/********************
+  Challenge: To make a function greet properly
+  
+   P - one string;
+   R - return , the right hello message;
+   E - ('Mr. Spock'), 'Hello, Mr. Spock';
+       ('Liutenant Uhura'), 'Hello, Liutenant Uhura';
+       
+   P - use a template literal to properly display the message.
+********************/
+
+function sayHello(name) {
+    return `Hello, ${name}`;
+}
