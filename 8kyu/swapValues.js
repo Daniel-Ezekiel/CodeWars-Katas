3c7289bfd3879d1596247c4f7ad9a77999d19367
Daniel Ezekiel <dbezekiel77@gmail.com>
2022-06-29 12:46:35 +0100

    new challenge completed

---
+++ 8kyu/swapValues.js
@@ -0,0 +1,13 @@
+/*****************************
+  Challenge: To pass an array into a function and swap the values
+  
+  P - an array
+  R - return a reversed or swapped version of the array;
+  E - arr[0], 2;
+      arr[1], 1;
+  P - call the reverse method on the array and that should solve the problem
+*****************************/
+
+function swapValues(arr) {
+    arr = arr.reverse();
+}
