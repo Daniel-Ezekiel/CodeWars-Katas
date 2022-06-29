8efec739886e12773659ca1a97d97c56b14e6a1d
Daniel Ezekiel <dbezekiel77@gmail.com>
2022-06-29 12:43:57 +0100

    new challenge completed

---
+++ 8kyu/printingArrayElementsWithCommaDelimiters.js
@@ -0,0 +1,14 @@
+/**********************
+  Challenge: To print an array in the same order with comma delimiters
+  
+  P - an array
+  R - return a string of the array elements with comma delimiters
+  E - [2,4,5,2], "2,4,5,2"
+  
+  P - call the join method on the array without any parameters or using the comma as a paramter to serve as a delimiter;
+**********************/
+
+function printArray(array){
+    //show me the code!
+    return array.join();
+}
