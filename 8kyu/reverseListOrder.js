aff26b711c472e5bc7ad3c2e7f45c6770784e4c0
Daniel Ezekiel <dbezekiel77@gmail.com>
2022-07-09 12:07:31 +0100

    new challenge completed

---
+++ 8kyu/reverseListOrder.js
@@ -0,0 +1,14 @@
+/***************************
+  Challenge: to return a list in reverse order;
+  
+  parameter is ans array.
+  return an array with its elements in the reverse order;
+  ex. => ([1,2,3,4]), [4,3,2,1];
+         ([3,1,5,4]), [4,5,1,3];
+         
+  my approach: call the reverse method on the array and return the results;         
+***************************/
+
+function reverseList(list) {
+    return list.reverse();
+}
