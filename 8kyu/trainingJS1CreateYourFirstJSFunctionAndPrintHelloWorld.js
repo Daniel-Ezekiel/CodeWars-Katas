6917e2831394a9fed46f07bb63733251b4cce691
Daniel Ezekiel <dbezekiel77@gmail.com>
2022-07-10 16:21:04 +0100

    challenge completed

---
+++ 8kyu/trainingJS1CreateYourFirstJSFunctionAndPrintHelloWorld.js
@@ -0,0 +1,21 @@
+/****************
+  Challlenge: to create a function that logs hello world to the console
+  
+  no parameter,
+  log 'Hello World!' to the console,
+  calling the helloWorld(), should log the right message,
+  
+  use an arrow function to create a function whose output is logged to the console
+  
+****************/
+// Following all set instructions;
+
+// function helloWorld(){
+//   let str = 'Hello World!';
+//   console.log(str);
+// }
+
+const helloWorld = _ => {
+    let str = 'Hello World!';
+    console.log(str);
+}
