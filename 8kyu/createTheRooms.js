92f0505f03a439481338acb7b08177d1be6ba24a
Daniel Ezekiel <dbezekiel77@gmail.com>
2022-08-09 13:53:45 +0100

    8kyu challenge completed

---
+++ 8kyu/createTheRooms.js
@@ -0,0 +1,18 @@
+// Add rooms here
+var rooms = {};
+
+rooms.name = {
+  room1: 'R1',
+  room2: 'R2',
+  room3: 'R3'
+};
+rooms.description={
+  room1: 'D1',
+  room2: 'D2',
+  room3: 'D3'  
+};
+rooms.completed={
+  room1: true,
+  room2: true,
+  room3: false
+};
