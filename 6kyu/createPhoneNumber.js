dccbca8cd9e8da0cdd4f192994907a4128fdea02
Daniel Ezekiel <dbezekiel77@gmail.com>
2023-01-03 20:02:49 +0100

    6kyu(String) - Create Phone Number challenge

---
+++ 6kyu/createPhoneNumber.js
@@ -0,0 +1,7 @@
+function createPhoneNumber(numbers){
+  const first = numbers.slice(0,3).join('');
+  const second = numbers.slice(3,6).join('');
+  const third = numbers.slice(6).join('');
+  
+  return `(${first}) ${second}-${third}`
+}
